e9c79704aa58244473db69e984b00c19029e0540
Gurtarn <gsingh1053@myseneca.ca>
2022-10-03 04:49:22 +0530

    initial commit

---
+++ data-service.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+
+var employees = [];
+var departments = [];
+
+module.exports.initialize = function () {
+
+    var PROMISE = new PROMISE((resolve, reject) => {
+       
+        try {
+
+            fs.readFile('./data/employees.json', (err, data) => {
+                if (err) throw err;
+
+                employees = JSON.parse(data);
+                console.log("initialize - load employees.");
+            })
+
+            fs.readFile('./data/departments.json', (err, data) => {
+                if (err) throw err;
+
+                departments = JSON.parse(data);
+                console.log("initialize - load departments.");
+            })
+
+        } catch (ex) {
+                      console.log("initialize - FAILURE.");
+                      reject("initialize - FAILURE.");
+                     }
+        console.log("initialize - SUCCESS.");
+        resolve("initialize - SUCCESS.");
+    })
+
+    return promise;
+};
+
+module.exports.getAllEmployees = function () {
+
+    var promise = new Promise((resolve, reject) => {
+        
+       //employees = [];
+       if(employees.length === 0) {
+        var err = "getAllEmployees() does not have any data.";
+        reject({message: err});
+       }  
+
+    resolve (employees);
+    })
+    return promise;
+};
+
+module.exports.getManagers = function () {
+
+    var lManagers = [];
+    var promise = new Promise((resolve, reject) => {
+      
+       for (var i=0; i < employees.length; i++){
+           if (employees[i].isManager == true) {
+           lManagers.push(employees[i]);
+           }
+       }
+
+       if(lManagers.length === 0) {
+        var err = "getManagers() does not have any data.";
+        reject({message: err});
+       }  
+
+    resolve (lManagers);
+    })
+    return promise;
+};
+
+module.exports.getDepartments = function () {
+
+    var promise = new Promise((resolve, reject) => {
+        if(departments.length === 0) {
+         var err = "getDepartments() does not have any data.";
+         reject({message: err});
+        }  
+ 
+     resolve (departments);
+     })
+     return promise;
+};
